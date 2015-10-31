08987526b1fa4ab85b6f6d3952f58d63b89fb38f
Akhilesh <ara.akhilesh@gmail.com>
2015-10-31 16:21:04 -0400

    Table insertion

--- nat.c
+++ nat.c
@@ -16,7 +16,13 @@
 #include <linux/skbuff.h>
 #include <linux/udp.h>
 
-#define PUBLIC_IP_ADDRESS_NAT "\xC0\xA8\x01\x01" //192.168.1.1
+#define PUBLIC_IP_ADDRESS_NAT "\xC0\xA8\x01\x01" // 192.168.1.1
+#define PUBLIC_VIDEO_PORT_NAT "\xC3\x50"         // 50000
+#define NUMBER_OF_SERVERS     4
+#define SERVER_IP_1           "\xC0\xA8\x01\x02" // 192.168.1.2
+#define SERVER_IP_2           "\xC0\xA8\x01\x03" // 192.168.1.3
+#define SERVER_IP_3           "\xC0\xA8\x01\x04" // 192.168.1.4
+#define SERVER_IP_4           "\xC0\xA8\x01\x05" // 192.168.1.5
 
 static struct nf_hook_ops netfilter_ops_in, netfilter_ops_out;
 static char *if_eth0 = "eth0";
@@ -30,21 +36,22 @@ typedef struct nat_table {
 	unsigned short client_port;
 	unsigned short server_port;
 	//timestamp to be done
+	struct list_head list;
 }nat_table;
 
-static int number_of_servers = 4;
-static nat_table *nat_table_ptr[10];
-int table_entries = 0;
+struct list_head nat_head;
+LIST_HEAD(nat_head);
 
+int table_entries = 0;
 
 nat_table * search_nat_table(unsigned int client_ipaddress, unsigned int client_port, int table_entries)
 {
-	int i = 0;
-	for(;i<table_entries;i++)
-	{
-		if((nat_table_ptr[i]->client_ip == client_ipaddress) && (nat_table_ptr[i]->client_ip == client_port))
-			return nat_table_ptr[i];
-	}
+//	int i = 0;
+//	for(;i<table_entries;i++)
+//	{
+//		if((nat_table_ptr[i]->client_ip == client_ipaddress) && (nat_table_ptr[i]->client_ip == client_port))
+//			return nat_table_ptr[i];
+//	}
 	return NULL;
 }
 
@@ -57,6 +64,7 @@ unsigned int dnat_hook(unsigned int hooknum,
 	unsigned int source_ip;
 	unsigned short source_port;
 	nat_table *rule; // A pointer to the structure of the corresponding rule
+	nat_table *temp;
 
 	/* Accept packets from servers */
 	if((strcmp(in->name, if_eth0) == 0) || 
@@ -72,21 +80,29 @@ unsigned int dnat_hook(unsigned int hooknum,
 	if(( (ip_hdr(sock_buff))->daddr ) == *(unsigned int *)PUBLIC_IP_ADDRESS_NAT)
 		return NF_DROP;
 	
-	/* Check if it's a UDP packet */
+	/* Check if it's a UDP packet and its destination port number */
 	if(( (ip_hdr(sock_buff))->protocol ) != 17){ return NF_ACCEPT; }
 
 	source_ip   =  ip_hdr(sock_buff)->saddr;
 	udp_header  =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
+	if((udp_header->dest) != *(unsigned short *)PUBLIC_VIDEO_PORT_NAT) { return NF_DROP; }
+
+	/* Search nat_table for client entry */
 	source_port =  udp_header->source; 
-	rule = search_nat_table(source_ip, source_port,table_entries); 
+	rule = search_nat_table(source_ip, source_port, table_entries); 
 	if(rule != NULL)
 	{
 		ip_hdr(sock_buff)->daddr = rule->server_ip;
-		udp_header->dest = rule->server_port;
 	}
 	else
 	{
 		//insert rule to nat_table and update dest addr and dest port
+		temp = kmalloc(sizeof(struct nat_table *), GFP_KERNEL);
+		temp->client_ip = source_ip;
+		temp->client_port = source_port;
+		temp->server_ip = *(unsigned int *)SERVER_IP_1;
+		ip_hdr(sock_buff)->daddr = temp->server_ip;
+		list_add(&temp->list, &nat_head);
 	}
 
 	/* Accept all other packets */
@@ -119,11 +135,10 @@ unsigned int snat_hook(unsigned int hooknum,
 	destination_ip   =  ip_hdr(sock_buff)->daddr;
 	udp_header       =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
 	destination_port =  udp_header->dest; 
-	rule = search_nat_table(destination_ip, destination_port,table_entries); 
+	rule = search_nat_table(destination_ip, destination_port, table_entries); 
 	if(rule != NULL)
 	{
 		ip_hdr(sock_buff)->saddr = *(unsigned int *)PUBLIC_IP_ADDRESS_NAT;
-		//udp_header->source = rule->server_port;
 	}
 	else
 	{
