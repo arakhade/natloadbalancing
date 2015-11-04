b3e26f1c403fa2405cb5be483e47a95284076957
TPan11 <pandetanaysoc@gmail.com>
2015-11-04 10:03:46 -0500

    Updated Nat and client server code

--- nat.c
+++ nat.c
@@ -16,22 +16,22 @@
 #include <linux/skbuff.h>
 #include <linux/udp.h>
 
-#define PUBLIC_IP_ADDRESS_NAT "\xC0\xA8\x01\x01" // 192.168.0.1
-#define PUBLIC_VIDEO_PORT_NAT "\xC3\x50"         // 50000
+#define PUBLIC_IP_ADDRESS_NAT "\x98\xA8\x00\x15" // 152.168.0.21
+#define PUBLIC_VIDEO_PORT_NAT "\x1F\x90"         // 8080
 #define NUMBER_OF_SERVERS     4
 
 char *server_ip[4]={	"\xC0\xA8\x01\x15", 	// 192.168.1.21
-			"\xC0\xA8\x01\x16", 	// 192.168.1.22
-			"\xC0\xA8\x01\x17",	// 192.168.1.23
-			"\xC0\xA8\x01\x18"};	// 192.168.1.24
+			"\xC0\xA8\x01\x15", 	// 192.168.1.22
+			"\xC0\xA8\x01\x15",	// 192.168.1.23
+			"\xC0\xA8\x01\x15"};	// 192.168.1.24
 static int round_robin_server_number = 0;
 
 static struct nf_hook_ops netfilter_ops_in, netfilter_ops_out;
 static char *if_eth0 = "eth0";
 static char *if_eth1 = "eth1";
-/*static char *if_eth2 = "eth2";
+static char *if_eth2 = "eth2";
 static char *if_eth3 = "eth3";
-static char *if_eth4 = "eth4";*/
+static char *if_eth4 = "eth4";
 struct sk_buff *sock_buff;
 struct udphdr *udp_header;
 
@@ -91,7 +91,7 @@ int	insert_nat_table_roundrobin(int source_ip,int source_port)
 }
 
 unsigned int dnat_hook(unsigned int hooknum,
-                       struct sk_buff **skb,
+                       struct sk_buff *skb,
                        const struct net_device *in,
                        const struct net_device *out,
                        int (*okfn) (struct sk_buff *))
@@ -101,43 +101,53 @@ unsigned int dnat_hook(unsigned int hooknum,
 	int udp_len;
 	nat_table *rule; // A pointer to the structure of the corresponding rule
 
+        printk("1\n");
 	/* Accept packets from servers */
 	if(in)
 	{
-		if((strcmp(in->name, if_eth0) == 0) || 
-			(strcmp(in->name, if_eth1) == 0)) 
+                printk("2\n");
+		if((strcmp(in->name, if_eth1) == 0) ||
+		   (strcmp(in->name, if_eth2) == 0) ||
+                   (strcmp(in->name, if_eth3) == 0) ||
+                   (strcmp(in->name, if_eth4) == 0)) 
 		{ 
-			return NF_ACCEPT; 
+		printk("3\n");
+		return NF_ACCEPT; 
 		}
 	}
-
-	/* Drop packet if destination IP is not public IP of NAT */
-	sock_buff = *skb;
-	if(!sock_buff){ return NF_ACCEPT; }
-	if(!(ip_hdr(sock_buff))){ return NF_ACCEPT; }
+	sock_buff = skb;
+	if(!sock_buff){ printk("4\n");
+	return NF_ACCEPT; }
+	if(!(ip_hdr(sock_buff))){ printk("5\n");
+	return NF_ACCEPT; }
 	if(( (ip_hdr(sock_buff))->daddr ) != *(unsigned int *)PUBLIC_IP_ADDRESS_NAT)
-		return NF_DROP;
+		{printk("6\n");
+	return NF_DROP;}
 	
 	/* Check if it's a UDP packet and its destination port number */
-	if(( (ip_hdr(sock_buff))->protocol ) != 17){ return NF_ACCEPT; }
+	if(( (ip_hdr(sock_buff))->protocol ) != 17){ printk("7\n");
+	return NF_ACCEPT; }
 
 	source_ip   =  ip_hdr(sock_buff)->saddr;
 	udp_header  =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
-	if((udp_header->dest) != *(unsigned short *)PUBLIC_VIDEO_PORT_NAT) { return NF_DROP; }
+	if((udp_header->dest) != *(unsigned short *)PUBLIC_VIDEO_PORT_NAT) { printk("8\n");
+	return NF_DROP; }
 
 	/* Search nat_table for client entry */
 	source_port =  udp_header->source; 
 	udp_len = (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
 	rule = search_nat_table(source_ip, source_port); 
-	
+	printk("9\n");
 	if(rule != NULL)
 	{
+                printk("10\n");
 		ip_hdr(sock_buff)->daddr = rule->server_ip;
 	}
 	else
-	{
+	{printk("11\n");
 		if(!(insert_nat_table_roundrobin(source_ip, source_port)))
-			return NF_DROP;
+		{printk("12\n");
+		return NF_DROP;}
 	}
 	udp_header->check = 0;
 	udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
@@ -149,13 +159,18 @@ unsigned int dnat_hook(unsigned int hooknum,
 							   0));
 	ip_hdr(sock_buff)->check = 0;
 	ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
-
+	printk("13\n");
 	/* Accept all other packets */
 	return NF_ACCEPT;
+
+		
+		
+	
+
 }
 
 unsigned int snat_hook(unsigned int hooknum,
-                       struct sk_buff **skb,
+                       struct sk_buff *skb,
                        const struct net_device *in,
                        const struct net_device *out,
                        int (*okfn) (struct sk_buff *))
@@ -164,32 +179,34 @@ unsigned int snat_hook(unsigned int hooknum,
 	unsigned short destination_port;
 	int udp_len;
 	nat_table *rule; // A pointer to the structure of the corresponding rule
-
+	printk("14\n");
 	/* Do not mangle packets other than from servers */
-	if(in)
-	{
-		if(!(strcmp(in->name, if_eth0) == 0) || 
-			!(strcmp(in->name, if_eth1) == 0)) 
-		{ 
+	if(out)
+	{printk("15\n");
+		if(!(strcmp(out->name, if_eth0) == 0)) 
+		{ printk("16\n");
 			return NF_ACCEPT; 
 		}
 	}
 
 	/* Check if it's a UDP packet */
-	sock_buff = *skb;
-	if(!sock_buff){ return NF_ACCEPT; }
-	if(!(ip_hdr(sock_buff))){ return NF_ACCEPT; }
-	if(( (ip_hdr(sock_buff))->protocol ) != 17){ return NF_ACCEPT; }
+	sock_buff = skb;
+printk("17\n");
+	if(!sock_buff){ printk("18\n");return NF_ACCEPT; }
+	if(!(ip_hdr(sock_buff))){ printk("19\n");return NF_ACCEPT; }
+	if(( (ip_hdr(sock_buff))->protocol ) != 17){printk("20\n"); return NF_ACCEPT; }
 
 	destination_ip   =  ip_hdr(sock_buff)->daddr;
 	udp_header       =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
 	destination_port =  udp_header->dest; 
 	udp_len          =  (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
 	rule = search_nat_table(destination_ip, destination_port); 
+printk("21\n");
 	if(rule == NULL)
-	{
+	{printk("22\n");
 		return NF_DROP; //drop packet if rule is not found in NAT table
 	}
+printk("23\n");
 	ip_hdr(sock_buff)->saddr = *(unsigned int *)PUBLIC_IP_ADDRESS_NAT;
 	udp_header->check = 0;
 	udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
@@ -201,7 +218,7 @@ unsigned int snat_hook(unsigned int hooknum,
 							   0));
 	ip_hdr(sock_buff)->check = 0;
 	ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
-
+printk("24\n");
 	return NF_ACCEPT;
 }
 
