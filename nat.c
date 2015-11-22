55c98b11de2288786da5d05c0aea0e47ad399eba
Satyajit <satyajitbm@gmail.com>
2015-11-21 21:13:52 -0500

    Add license. Remove unnecessary printks. Add kfree in cleanup_module. Make if_ethX constant

--- nat.c
+++ nat.c
@@ -20,21 +20,24 @@
 #define PUBLIC_VIDEO_PORT_NAT "\x1F\x90"         // 8080
 #define NUMBER_OF_SERVERS     4
 
-static char *server_ip[4]={"\xC0\xA8\x01\x1", 	// 192.168.1.1
-					"\xC0\xA8\x01\x1", 	// 192.168.2.1
-					"\xC0\xA8\x01\x1",	// 192.168.3.1
-					"\xC0\xA8\x01\x1"};	// 192.168.4.1
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("NAT with round robin load balancing");
 
-static int round_robin_server_number = 0;
+static const char *server_list[4]={"\xC0\xA8\x01\x1", 	// 192.168.1.1
+			   "\xC0\xA8\x01\x1", 	// 192.168.2.1
+			   "\xC0\xA8\x01\x1",	// 192.168.3.1
+			   "\xC0\xA8\x01\x1"};	// 192.168.4.1
+
 static struct nf_hook_ops netfilter_ops_in, netfilter_ops_out;
-static char *if_eth0 = "eth0";
-static char *if_eth1 = "eth1";
-static char *if_eth2 = "eth2";
-static char *if_eth3 = "eth3";
-static char *if_eth4 = "eth4";
-static int table_entries = 0;
+static const char *if_eth0 = "eth0";
+static const char *if_eth1 = "eth1";
+static const char *if_eth2 = "eth2";
+static const char *if_eth3 = "eth3";
+static const char *if_eth4 = "eth4";
 static struct sk_buff *sock_buff;
 static struct udphdr *udp_header;
+static int round_robin_server_number = 0;
+static int table_entries = 0;
 
 
 typedef struct nat_table {
@@ -54,7 +57,6 @@ typedef struct nat_table {
 
 static nat_table nat_head;
 
-
 nat_table * search_nat_table(unsigned int client_ipaddress, unsigned int client_port)
 {
 	nat_table *table_row;
@@ -72,23 +74,25 @@ nat_table * search_nat_table(unsigned int client_ipaddress, unsigned int client_
 	return NULL;
 }
 
-int	insert_nat_table_roundrobin(int source_ip,int source_port)
+int insert_nat_table_roundrobin(int source_ip,int source_port)
 {	
 	nat_table * temp;
-	//return 0 if not possible else retun 1;
 	temp = kmalloc(sizeof(struct nat_table *), GFP_KERNEL);
-	if(temp == NULL)
+	if(!temp)
+	{
+		printk(KERN_ERR "kmalloc failed\n");
 		return 0;
+	}
 	temp->client_ip = source_ip;
 	temp->client_port = source_port;
-	temp->server_ip = *(unsigned int *)server_ip[round_robin_server_number];
+	temp->server_ip = *(unsigned int *)server_list[round_robin_server_number];
 	if(!sock_buff)
 		return 0;
-	else if(!ip_hdr(sock_buff))
+	if(!ip_hdr(sock_buff))
 		return 0;
 	ip_hdr(sock_buff)->daddr = temp->server_ip;
 	list_add_tail(&temp->list, &nat_head.list);
-	round_robin_server_number = (round_robin_server_number + 1) % 4;
+	round_robin_server_number = (round_robin_server_number + 1) % NUMBER_OF_SERVERS;
 	return 1;
 }
 
@@ -103,94 +107,91 @@ unsigned int dnat_hook(unsigned int hooknum,
 	int udp_len;
 	nat_table *rule; // A pointer to the structure of the corresponding rule
 
-	printk("1\n");
 	/* Accept packets from servers */
 	if(in)
 	{
-		printk("2\n");
 		if((strcmp(in->name, if_eth1) == 0) ||
 			(strcmp(in->name, if_eth2) == 0) ||
 			(strcmp(in->name, if_eth3) == 0) ||
 			(strcmp(in->name, if_eth4) == 0)) 
 		{ 
-			printk("3\n");
 			return NF_ACCEPT; 
 		}
-	}
-	sock_buff = skb;
-	if(!sock_buff)
-	{
-		printk("4\n");
-		return NF_ACCEPT; 
-	}
-	if(!(ip_hdr(sock_buff)))
-	{
-		printk("5\n");
-		return NF_ACCEPT; 
-	}
-	if(!( (ip_hdr(sock_buff))->daddr ))
-	{
-		printk("6\n");
-		return NF_ACCEPT;
-	}
-	if(( (ip_hdr(sock_buff))->daddr ) != *(unsigned int *)PUBLIC_IP_ADDRESS_NAT)
-	{
-		printk("7\n");
-		return NF_DROP;
-	}
-	if(!( (ip_hdr(sock_buff))->protocol ))
-	{
-		return NF_ACCEPT;
-	}
-	/* Check if it's a UDP packet and its destination port number */
-	if(( (ip_hdr(sock_buff))->protocol ) != 17)
-	{
-		printk("8\n");
-		return NF_ACCEPT;
-	}
-	
-	source_ip   =  ip_hdr(sock_buff)->saddr;
-	udp_header  =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
-	if(!udp_header)
-	{
-		printk("9\n");
-		return NF_ACCEPT;
-	}
-	if((udp_header->dest) != *(unsigned short *)PUBLIC_VIDEO_PORT_NAT) 
-	{
-		printk("10\n");
-		return NF_DROP; 
-	}
-	/* Search nat_table for client entry */
-	source_port =  udp_header->source; 
-	udp_len = (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
-	rule = search_nat_table(source_ip, source_port); 
-	printk("11\n");
-	if(rule != NULL)
-	{
-		printk("12\n");
-		ip_hdr(sock_buff)->daddr = rule->server_ip;
-	}
-	else
-	{
-		printk("13\n");
-		if(!(insert_nat_table_roundrobin(source_ip, source_port)))
+		sock_buff = skb;
+		if(!sock_buff)
 		{
-			printk("13\n");
+			printk(KERN_ERR "Socket buffer not initialized\n");
+			return NF_ACCEPT; 
+		}
+		if(!(ip_hdr(sock_buff)))
+		{
+			printk(KERN_ERR "IP header not initialized\n");
+			return NF_ACCEPT; 
+		}
+		if(!( (ip_hdr(sock_buff))->daddr ))
+		{
+			printk(KERN_ERR "Destination IP address not initialized\n");
+			return NF_ACCEPT;
+		}
+		if(( (ip_hdr(sock_buff))->daddr ) != *(unsigned int *)PUBLIC_IP_ADDRESS_NAT)
+		{
+			printk(KERN_INFO "Dropped. Cause: Not destined to public IP of NAT");
 			return NF_DROP;
 		}
+		if(!( (ip_hdr(sock_buff))->protocol ))
+		{
+			return NF_ACCEPT;
+		}
+		/* Check if it's a UDP packet and its destination port number */
+		if(( (ip_hdr(sock_buff))->protocol ) != 17)
+		{
+			return NF_ACCEPT;
+		}
+	
+		source_ip   =  ip_hdr(sock_buff)->saddr;
+		udp_header  =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
+		if(!udp_header)
+		{
+			printk(KERN_ERR "UDP header not initialized\n");
+			return NF_ACCEPT;
+		}
+		if((udp_header->dest) != *(unsigned short *)PUBLIC_VIDEO_PORT_NAT) 
+		{
+			printk(KERN_INFO "Dropped. Cause: Not destined to public port of NAT\n");
+			return NF_DROP; 
+		}
+		/* Search nat_table for client entry */
+		source_port =  udp_header->source; 
+		udp_len = (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
+		rule = search_nat_table(source_ip, source_port); 
+		if(rule != NULL)
+		{
+			ip_hdr(sock_buff)->daddr = rule->server_ip;
+		}
+		else
+		{
+			if(!(insert_nat_table_roundrobin(source_ip, source_port)))
+			{
+				printk(KERN_ERR "Could not insert rule to NAT table\n");
+				return NF_DROP;
+			}
+		}
+		udp_header->check = 0;
+		udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
+						      ip_hdr(sock_buff)->daddr,
+						      udp_len,
+						      IPPROTO_UDP,
+						      csum_partial((unsigned char *)udp_header,
+						      udp_len,
+								   0));
+		ip_hdr(sock_buff)->check = 0;
+		ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
 	}
-	udp_header->check = 0;
-	udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
-					      ip_hdr(sock_buff)->daddr,
-					      udp_len,
-					      IPPROTO_UDP,
-					      csum_partial((unsigned char *)udp_header,
-					      udp_len,
-							   0));
-	ip_hdr(sock_buff)->check = 0;
-	ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
-	printk("13\n");
+	else // if(in)
+	{
+		printk(KERN_ERR "Error:Input interface not initialized\n");
+	}
+
 	/* Accept all other packets */
 	return NF_ACCEPT;
 }
@@ -205,72 +206,71 @@ unsigned int snat_hook(unsigned int hooknum,
 	unsigned short destination_port;
 	int udp_len;
 	nat_table *rule; // A pointer to the structure of the corresponding rule
-	printk("14\n");
 	/* Do not mangle packets other than from servers */
 	if(out)
 	{
-		printk("15\n");
 		if(!(strcmp(out->name, if_eth0) == 0)) 
 		{
-			printk("16\n");
 			return NF_ACCEPT; 
 		}
-	}
 
-	/* Check if it's a UDP packet */
-	sock_buff = skb;
-	printk("17\n");
-	if(!sock_buff)
-	{
-		printk("18\n");
-		return NF_ACCEPT;
-	}
-	if(!(ip_hdr(sock_buff)))
-	{
-		printk("19\n");
-		return NF_ACCEPT;
-	}
-	if(( (ip_hdr(sock_buff))->protocol ) != 17)
-	{
-		printk("20\n"); 
-		return NF_ACCEPT; 
-	}
-	destination_ip   =  ip_hdr(sock_buff)->daddr;
-	udp_header       =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
-	if(!udp_header)
-	{
-		return NF_ACCEPT;
-	}
-	destination_port =  udp_header->dest; 
-	udp_len          =  (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
-	rule = search_nat_table(destination_ip, destination_port); 
-	printk("21\n");
-	if(rule == NULL)
-	{
-		printk("22\n");
-		return NF_DROP; //drop packet if rule is not found in NAT table
-	}
-	printk("23\n");
-	ip_hdr(sock_buff)->saddr = *(unsigned int *)PUBLIC_IP_ADDRESS_NAT;
-	udp_header->check = 0;
-	udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
-					    ip_hdr(sock_buff)->daddr,
-					    udp_len,
-					    IPPROTO_UDP,
-					    csum_partial((unsigned char *)udp_header,
-						udp_len,
-						0));
+		/* Check if it's a UDP packet */
+		sock_buff = skb;
+		if(!sock_buff)
+		{
+			printk(KERN_ERR "Socket buffer not initialized\n");
+			return NF_ACCEPT;
+		}
+		if(!(ip_hdr(sock_buff)))
+		{
+			printk(KERN_ERR "IP header not initialized\n");
+			return NF_ACCEPT;
+		}
+		if(( (ip_hdr(sock_buff))->protocol ) != 17)
+		{
+			return NF_ACCEPT; 
+		}
+		destination_ip   =  ip_hdr(sock_buff)->daddr;
+		udp_header       =  (struct udphdr *)(sock_buff->data + (( (ip_hdr(sock_buff))->ihl ) * 4));
+		if(!udp_header)
+		{
+			printk(KERN_ERR "UDP header not initialized\n");
+			return NF_ACCEPT;
+		}
+		destination_port =  udp_header->dest; 
+		udp_len          =  (sock_buff->len - (ip_hdr(sock_buff)->ihl << 2));
+		rule = search_nat_table(destination_ip, destination_port); 
+		if(rule == NULL)
+		{
+			printk(KERN_ERR "Rule not found in NAT table\n");
+			return NF_DROP; //drop packet if rule is not found in NAT table
+		}
+		ip_hdr(sock_buff)->saddr = *(unsigned int *)PUBLIC_IP_ADDRESS_NAT;
+		udp_header->check = 0;
+		udp_header->check = csum_tcpudp_magic(ip_hdr(sock_buff)->saddr,
+						    ip_hdr(sock_buff)->daddr,
+						    udp_len,
+						    IPPROTO_UDP,
+						    csum_partial((unsigned char *)udp_header,
+							udp_len,
+							0));
 						
-	ip_hdr(sock_buff)->check = 0;
-	ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
-	printk("24\n");
-	return NF_ACCEPT;
+		ip_hdr(sock_buff)->check = 0;
+		ip_hdr(sock_buff)->check = ip_fast_csum(ip_hdr(sock_buff), ip_hdr(sock_buff)->ihl);
+	}
+	else // if(out)
+	{
+		printk(KERN_ERR "Error:Output interface not initialized\n");
+	}
+		printk("24\n");
+		return NF_ACCEPT;
 }
 int init_module()
 {
 	LIST_HEAD(nat_head);
+
 	netfilter_ops_in.hook		=		(nf_hookfn *)dnat_hook;
-	netfilter_ops_in.pf			=		PF_INET;
+	netfilter_ops_in.pf		=		PF_INET;
 	netfilter_ops_in.hooknum	=		NF_INET_PRE_ROUTING;
 	netfilter_ops_in.priority	=		NF_IP_PRI_FIRST;
 
@@ -287,6 +287,16 @@ int init_module()
 
 void cleanup_module()
 {
+	struct list_head *p, *q;
+	struct nat_table *an_entry;
+
 	nf_unregister_hook(&netfilter_ops_in);
 	nf_unregister_hook(&netfilter_ops_out);
+
+	list_for_each_safe(p, q, &nat_head.list)
+	{
+		an_entry = list_entry(p, struct nat_table, list);
+		list_del(p);
+		kfree(an_entry);
+	}
 }
